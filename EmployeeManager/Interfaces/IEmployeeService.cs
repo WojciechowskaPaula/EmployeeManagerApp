6e7da8891aaa68ce3b3d02d3bc269107757f384a
Paula <paulla.wojciechowska@gmail.com>
2021-12-09 15:53:22 +0100

    Add implementation to EmployeeService

---
+++ EmployeeManager/Interfaces/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Interfaces
+{
+    public interface IEmployeeService
+    {
+        List<Employee> GetAllEmployees();
+    }
+}
--- EmployeeManager/Services/EmployeeService.cs
+++ EmployeeManager/Services/EmployeeService.cs
@@ -1,14 +1,25 @@
 ﻿using EmployeeManager.Data;
+using EmployeeManager.Interfaces;
 using EmployeeManager.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace EmployeeManager.Services
 {
-    public class EmployeeService
+    public class EmployeeService : IEmployeeService
     {
-       
+        private readonly ApplicationDbContext _dbContext;
+        public EmployeeService(ApplicationDbContext applicationDbContext)
+        {
+            _dbContext = applicationDbContext;
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            var employees = _dbContext.Employees.ToList();
+            return employees;
+        }
     }
+
+    
 }
