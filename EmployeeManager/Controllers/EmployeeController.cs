a543bfc30153602330906f752d837c8115bdbc9a
Paula <paulla.wojciechowska@gmail.com>
2021-12-09 16:00:55 +0100

    Add GetAllEmployees method and view for it

---
+++ EmployeeManager/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeManager.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public IActionResult Index()
+        {
+            var employees = _employeeService.GetAllEmployees();
+            return View(employees);
+        }
+    }
+}
