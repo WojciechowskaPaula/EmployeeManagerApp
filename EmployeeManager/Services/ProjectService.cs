44b0e6d81b44f68e1a35ca4fb84b7ea26d7a2cbb
Paula <paulla.wojciechowska@gmail.com>
2021-12-13 16:18:35 +0100

    Create ProjectService

---
+++ EmployeeManager/Services/ProjectService.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeManager.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Services
+{
+    public class ProjectService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ProjectService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+    }
+}
