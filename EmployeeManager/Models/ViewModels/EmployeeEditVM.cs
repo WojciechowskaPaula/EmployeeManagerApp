bc21fce3cd5cba92daa99f8ce6b6f69fe643d179
Paula <paulla.wojciechowska@gmail.com>
2021-12-13 22:23:32 +0100

    Add EditVM class

---
+++ EmployeeManager/Models/ViewModels/EmployeeEditVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Models.ViewModels
+{
+    public class EmployeeEditVM
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDate { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+        public int ManagerId { get; set; }
+        public Position Position { get; set; }
+        public List<Project> Projects { get; set; }
+    }
+}
