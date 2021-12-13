93afd5c5910d45407641d0a55d37191274a28b7e
Paula <paulla.wojciechowska@gmail.com>
2021-12-13 22:24:56 +0100

    Add ProjectController

---
+++ EmployeeManager/Controllers/ProjectController.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeManager.Interfaces;
+using EmployeeManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly IProjectService _projectService;
+        
+        public ProjectController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
