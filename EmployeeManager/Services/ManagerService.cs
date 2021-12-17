851e1895db9801b3686132ef107464e970cb5c42
Paula <paulla.wojciechowska@gmail.com>
2021-12-17 17:20:40 +0100

    Create ManagerService

---
+++ EmployeeManager/Services/ManagerService.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeManager.Data;
+using EmployeeManager.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Services
+{
+    public class ManagerService : IManagerService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public ManagerService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        
+    }
+}
