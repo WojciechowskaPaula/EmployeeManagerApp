51342c5e98435c18ad50a1f88d1b3701ca772369
Paula <paulla.wojciechowska@gmail.com>
2022-01-01 21:17:47 +0100

    Delete Home controller and views

--- EmployeeManager/Startup.cs
+++ EmployeeManager/Startup.cs
@@ -58,7 +58,7 @@ namespace EmployeeManager
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Employee}/{action=Index}/{id?}");
             });
         }
     }
