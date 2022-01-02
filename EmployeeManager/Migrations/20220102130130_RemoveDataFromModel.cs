92479c1d4ebed07a95e2abf0ad7f8f91e0f9234d
Paula <paulla.wojciechowska@gmail.com>
2022-01-02 14:40:25 +0100

    Remove data from JobHistory table

---
+++ EmployeeManager/Migrations/20220102130130_RemoveDataFromModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManager.Migrations
+{
+    public partial class RemoveDataFromModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "JobHistories");
+
+            migrationBuilder.DropColumn(
+                name: "HireDate",
+                table: "JobHistories");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "JobHistories",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "HireDate",
+                table: "JobHistories",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
--- EmployeeManager/Models/JobHistory.cs
+++ EmployeeManager/Models/JobHistory.cs
@@ -10,8 +10,6 @@ namespace EmployeeManager.Models
         
         public int JobHistoryId { get; set; }
         ICollection <Position> Positions { get; set; }
-        public DateTime HireDate { get; set; }
-        public DateTime? EndDate { get; set; }
         public double Salary { get; set; }
         public Employee Employee { get; set; }
         public int EmployeeId { get; set; }
